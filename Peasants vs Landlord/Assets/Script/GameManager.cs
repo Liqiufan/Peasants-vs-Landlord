abcb90b9cadd78c0e61eafa0ae58b4f1abdc5fb8
Liqiufan <1052174866@qq.com>
2018-01-03 23:31:11 +0800

    Project

---
+++ Peasants vs Landlord/Assets/Script/GameManager.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour {
+    //private bool landlord = false;
+    private int landlordIndex;
+    private UISprite landlord;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            GameObject.Find("UI Root/Player02").GetComponent<UISprite>().spriteName = "Character_Landlord";
+        }
+	}
+}
