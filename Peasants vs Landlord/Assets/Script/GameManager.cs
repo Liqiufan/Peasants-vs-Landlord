8c93595e6bd269160914a74a09944b0946f75d31
Liqiufan <1052174866@qq.com>
2018-01-04 23:45:42 +0800

    Judge winner

--- Peasants vs Landlord/Assets/Script/GameManager.cs
+++ Peasants vs Landlord/Assets/Script/GameManager.cs
@@ -6,16 +6,17 @@ public class GameManager : MonoBehaviour {
     //private bool landlord = false;
     private int landlordIndex;
     private UISprite landlord;
+    private int[] playerCards = { 17, 17, 17 };
 	// Use this for initialization
 	void Start () {
-		
-	}
+        landlordIndex = Random.Range(1, 4);
+        GameObject.Find("UI Root/Players/Player0" + landlordIndex).GetComponent<UISprite>().spriteName = "Character_Landlord";
+        playerCards[landlordIndex - 1] += 3;
+        Debug.Log("Player0" + landlordIndex + " is landlord");
+    }
 	
 	// Update is called once per frame
 	void Update () {
-        if (Input.GetKeyDown(KeyCode.A))
-        {
-            GameObject.Find("UI Root/Player02").GetComponent<UISprite>().spriteName = "Character_Landlord";
-        }
+        
 	}
 }
---
+++ Peasants vs Landlord/Assets/Script/PlayerController.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour
+{
+    public Transform playerCards;
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    /// <summary>
+    /// PlayCard when the player click the button "出牌"
+    /// </summary>
+    public void PlayCard()
+    {
+        Destroy(playerCards.GetChild(0).gameObject);
+        JudgeWinner(playerCards.childCount);
+    }
+
+    /// <summary>
+    /// Judge the player is winner or not
+    /// </summary>
+    /// <param name="remainCards">Player's remains cards count</param>
+    private void JudgeWinner(int remainCards)
+    {
+        if(remainCards == 1)
+        {
+            Debug.Log("Player win the term!");
+        }
+    }
+}
